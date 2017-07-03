5af0b799319e72c1be47ad7705b18cec84dbda6d
dw8855 <314585797@qq.com>
2017-07-03 16:59:37 +0800

    调用广电的.so文件

--- ocrCourt/imagePretreatmentGuang.py
+++ ocrCourt/imagePretreatmentGuang.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import os
 import platform
+from ctypes import *
 
 #只处理window以及linux系统的32位以及64位
 
@@ -11,12 +12,25 @@ import platform
 def imagePretreatmentGuang(imagePath, saveImagePath):
     if platform.system() == 'Linux':
         if platform.machine() == 'x86_64':
+            ocr = cdll.LoadLibrary('./libgrgPageAnalysisV3.so')
+            pageAnalysis = ocr.grgPageAnalysis
+            arg0 = imagePath
+            arg1 = saveImagePath + '/' + imagePath.split('/')[-1]
+            arg2 = '123'
+            pageAnalysis(arg0, arg1, arg2)
+            imageFile = Image.open(arg1)
+            return imageFile
+        ''' 
+            直接调用.so文件比用几次io接口好很多
+            
+            
             arg0 = imagePath
             arg1 = saveImagePath + '/' +imagePath.split('/')[-1]
             arg2 = 123
             os.system('./pageAnalysis ' + arg0 + ' ' + arg1 + ' ' + arg2)
             imageFile = Image.open(arg1)
             return imageFile
+        '''
         else:
             pass
     elif platform.system() == 'Windows':
