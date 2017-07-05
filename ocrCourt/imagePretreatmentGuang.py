071d02d7d834e2fd0e916b3cfb73970338c4454a
dw8855 <314585797@qq.com>
2017-07-05 15:57:35 +0800

    调用广电预处理新增windows选项（已通过测试），删除两个版本的32位

--- ocrCourt/imagePretreatmentGuang.py
+++ ocrCourt/imagePretreatmentGuang.py
@@ -1,29 +1,30 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 
 from PIL import Image
 import os
 import platform
 from ctypes import *
+import subprocess
 
-#只处理window以及linux系统的32位以及64位
+
+# 只处理window以及linux系统的32位以及64位
 
 
 
 def imagePretreatmentGuang(imagePath, saveImagePath):
     if platform.system() == 'Linux':
-        if platform.machine() == 'x86_64':
-            ocr = cdll.LoadLibrary('./libgrgPageAnalysisV3.so')
-            pageAnalysis = ocr.grgPageAnalysis
-            arg0 = imagePath
-            arg1 = saveImagePath + '/' + imagePath.split('/')[-1]
-            arg2 = '123'
-            pageAnalysis(arg0, arg1, arg2)
-            imageFile = Image.open(arg1)
-            return imageFile
+        ocr = cdll.LoadLibrary('./libgrgPageAnalysisV3.so')
+        pageAnalysis = ocr.grgPageAnalysis
+        arg0 = imagePath
+        arg1 = saveImagePath + '/' + imagePath.split('/')[-1]
+        arg2 = '123'
+        pageAnalysis(arg0, arg1, arg2)
+        imageFile = Image.open(arg1)
+        return imageFile
         ''' 
             直接调用.so文件比用几次io接口好很多
-            
-            
+
+
             arg0 = imagePath
             arg1 = saveImagePath + '/' +imagePath.split('/')[-1]
             arg2 = 123
@@ -31,11 +32,22 @@ def imagePretreatmentGuang(imagePath, saveImagePath):
             imageFile = Image.open(arg1)
             return imageFile
         '''
-        else:
-            pass
     elif platform.system() == 'Windows':
-        if platform.machine() == 'x86_64':
-            pass
-        else:
-            pass
+        arg0 = imagePath
+        arg1 = saveImagePath + os.path.sep + imagePath.split(os.path.sep)[-1]
+        arg2 = '123'
+        exePath = os.path.join('D:', os.sep, 'release')
+        exePath = os.path.join(exePath, 'pageAnalysis.exe')
+        subprocess.call([exePath, arg0, arg1, arg2])
+        # pageAnalysis = r'C:/Users/Administrator/Desktop/release/pageAnalysis.exe ' + arg0 + ' ' + arg1 + ' ' + arg2
+        # key = os.system('C:/Users/Administrator/Desktop/release/pageAnalysis.exe ' + arg0 + ' ' + arg1 + ' ' + arg2)
+        imageFile = Image.open(arg1)
+        return imageFile
 
+
+if __name__ == '__main__':
+    a = platform.system()
+    imagePath = os.path.join(os.path.join('D:', os.path.sep), 'page_1.jpg')
+    saveImagePath = os.path.join(os.path.join('D:', os.path.sep), 'down')
+    image = imagePretreatmentGuang(imagePath, saveImagePath)
+    # print(image)
